f7662da1844aa478002c6fb89d4a368d5c60edce
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-06-12 19:02:31 +0700

    Add combined TikTok page

---
+++ cicero-dashboard/app/tiktok/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+import TiktokInfoPage from "../info/tiktok/page";
+import TiktokPostAnalysisPage from "../posts/tiktok/page";
+
+export default function TiktokCombinedPage() {
+  return (
+    <>
+      <TiktokInfoPage />
+      <TiktokPostAnalysisPage />
+    </>
+  );
+}
