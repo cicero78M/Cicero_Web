2ac24f79a62c9947c0b4ab46aa87ecee74040801
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-06-14 11:40:28 +0700

    Fix WordCloud size initialization

--- cicero-dashboard/components/WordCloudChart.jsx
+++ cicero-dashboard/components/WordCloudChart.jsx
@@ -7,7 +7,7 @@ export default function WordCloudChart({ words = [] }) {
   const options = { rotations: 2, rotationAngles: [0, 0], fontSizes: [12, 40] };
   return (
     <div style={{ height: 300 }} className="w-full">
-      <ReactWordcloud words={words} options={options} />
+      <ReactWordcloud words={words} options={options} size={[300, 300]} />
     </div>
   );
 }
