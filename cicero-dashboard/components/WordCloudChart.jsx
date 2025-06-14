018eb77889d2490fffd13323598dc3c39065bc24
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-06-14 12:26:51 +0700

    Adjust WordCloud alignment

--- cicero-dashboard/components/WordCloudChart.jsx
+++ cicero-dashboard/components/WordCloudChart.jsx
@@ -4,7 +4,10 @@ import WordCloud from "react-d3-cloud";
 export default function WordCloudChart({ words = [] }) {
   if (typeof window === "undefined") return null;
   return (
-    <div style={{ height: 300 }} className="w-full overflow-hidden">
+    <div
+      style={{ height: 300 }}
+      className="w-full overflow-hidden flex justify-center items-start"
+    >
       <WordCloud data={words} width={300} height={300} />
     </div>
   );
