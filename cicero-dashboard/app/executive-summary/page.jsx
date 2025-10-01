824bf9fd02e8a2f8cd03e808fcd9f41b984f54a9
papiqo <nationalite45@gmail.com>
2025-10-01 19:20:31 +0700

    Update

--- cicero-dashboard/app/executive-summary/page.jsx
+++ cicero-dashboard/app/executive-summary/page.jsx
@@ -942,7 +942,7 @@ const normalizeUserKeyFromRecord = (record = {}) => {
   return null;
 };
 
-export const computeActivityBuckets = ({
+const computeActivityBuckets = ({
   users,
   likes,
   comments,
